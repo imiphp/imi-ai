26e9db9c3fa69ec141efdee56fed074b0202a70f
Yurun <admin@yurunsoft.com>
2023-07-04 16:55:43 +0800

    格式化代码

--- server/Module/Config/Enum/Configs.php
+++ server/Module/Config/Enum/Configs.php
@@ -11,7 +11,7 @@ abstract class Configs extends BaseEnum
 {
     #[EnumItem(['text' => '接口地址', 'default' => null])]
     public const API_URL = 'api_url';
-    
+
     #[EnumItem(['text' => '前端地址', 'default' => null])]
     public const WEB_URL = 'web_url';
 }
