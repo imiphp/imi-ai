01626b9ff613fd898cad1b507d76d2724c1953da
Yurun <admin@yurunsoft.com>
2023-10-19 16:12:03 +0800

    修复接口管理

--- admin/src/views/config/api_manager/index.vue
+++ admin/src/views/config/api_manager/index.vue
@@ -1,98 +1,176 @@
 <template>
   <div class="overflow-hidden">
     <n-card title="接口管理" :bordered="false" class="h-full rounded-8px shadow-sm overflow-auto">
-      <n-scrollbar y-scrollable class="max-h-full">
-        <n-table :single-line="false" striped class="w-max min-w-full api-manager-table">
-          <thead>
-            <tr class="text-center">
-              <th width="120">名称</th>
-              <th width="200">接口地址</th>
-              <th width="200">密钥</th>
-              <th width="200">代理</th>
-              <th width="250">信息</th>
-              <th width="150">熔断</th>
-              <th width="150">操作</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(item, index) in config['config:openai']?.config.apis" :key="index">
-              <td>{{ item.name }}</td>
-              <td>
-                <p v-for="(value, i) in item.baseUrls" :key="i">{{ value }}</p>
-              </td>
-              <td>
-                <p v-for="(value, i) in item.apiKeys" :key="i">{{ value }}</p>
-              </td>
-              <td>
-                <p v-for="(value, i) in item.proxys" :key="i">{{ value }}</p>
-              </td>
-              <td>
-                <p>
-                  <b>限流条件：</b>
-                  <template v-if="item.rateLimitAmount > 0">
-                    {{ item.rateLimitAmount }}次/{{ getRateLimitLabel(item.rateLimitUnit) }}
-                  </template>
-                  <template v-else>未启用</template>
-                </p>
-                <p v-if="item.leftRateLimitAmount > 0">
-                  <b>限流剩余次数：</b>
-                  {{ item.leftRateLimitAmount }}次
-                </p>
-                <p>
-                  <b>模型：</b>
-                  <template v-if="item.models.length === 0">所有</template>
-                </p>
-                <template v-if="item.models.length > 0">
-                  <p v-for="(value, i) in item.models" :key="i">{{ value }}</p>
-                </template>
-                <p>
-                  <b>客户端：</b>
-                  {{ item.client }}
-                </p>
-              </td>
-              <td>
-                <p>
-                  <b>熔断条件：</b>
-                  <template v-if="item.circuitBreaker.limitAmount > 0">
-                    {{ item.circuitBreaker.limitAmount }}次/{{ getRateLimitLabel(item.circuitBreaker.limitUnit) }}
-                  </template>
-                  <template v-else>未启用</template>
-                </p>
-                <p v-if="item.circuitBreaker.limitAmount > 0">
-                  <b>剩余次数：</b>
-                  {{ item.circuitBreaker.leftLimitAmount }}次
-                </p>
-                <p>
-                  <b>熔断时长：</b>
-                  {{ timespanHuman(item.circuitBreaker.breakDuration) }}
-                </p>
-                <p v-if="new Date().getTime() / 1000 < item.circuitBreaker.availableBeginTime">
-                  <b>熔断解除时间：</b>
-                  {{ new Date(item.circuitBreaker.availableBeginTime * 1000).toLocaleString() }}
-                </p>
-              </td>
-              <td>
-                <n-space justify="center">
-                  <n-switch v-model:value="item.enable">
-                    <template #checked>启用</template>
-                    <template #unchecked>禁用</template>
-                  </n-switch>
-                </n-space>
-              </td>
-            </tr>
-          </tbody>
-        </n-table>
-      </n-scrollbar>
+      <n-data-table :columns="columns" :data="list" scroll-x="1280" remote class="flex-1-hidden" />
     </n-card>
   </div>
 </template>
 
 <script setup lang="tsx">
-import { onMounted, ref } from 'vue';
-import { getConfig } from '~/src/service';
+import type { Ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
+import type { DataTableColumns } from 'naive-ui';
+import { useDialog } from 'naive-ui';
+import { getConfig, saveConfig } from '~/src/service';
 import { getRateLimitLabel, timespanHuman } from '~/src/utils';
 
+const dialog = useDialog();
+
 const config = ref<any>({});
+
+const list = computed(() => {
+  return config.value['config:openai']?.config.apis;
+});
+
+const columns: Ref<DataTableColumns<any>> = ref([
+  {
+    title: '名称',
+    key: 'name',
+    width: 120
+  },
+  {
+    title: '接口地址',
+    key: 'baseUrls',
+    width: 200,
+    render: row => {
+      const result = [];
+      for (const value of row.baseUrls) {
+        result.push(<p>{value}</p>);
+      }
+      return result;
+    }
+  },
+  {
+    title: '密钥',
+    key: 'apiKeys',
+    width: 200,
+    render: row => {
+      const result = [];
+      for (const value of row.apiKeys) {
+        result.push(<p>{value}</p>);
+      }
+      return result;
+    }
+  },
+  {
+    title: '信息',
+    key: 'info',
+    width: 250,
+    render: row => {
+      let leftRateLimitAmount;
+      if (row.leftRateLimitAmount > 0) {
+        leftRateLimitAmount = (
+          <p>
+            <b>限流剩余次数：</b>
+            {row.leftRateLimitAmount}次
+          </p>
+        );
+      }
+      let models;
+      if (row.models.length === 0) {
+        models = '所有';
+      } else {
+        models = [];
+        for (const value of row.models) {
+          models.push(<p>{value}</p>);
+        }
+      }
+      return (
+        <>
+          <p>
+            <b>限流条件：</b>
+            {row.rateLimitAmount > 0 ? `${row.rateLimitAmount}次/${getRateLimitLabel(row.rateLimitUnit)}` : '未启用'}
+          </p>
+          {leftRateLimitAmount}
+          <p>
+            <b>模型：</b>
+            {models}
+          </p>
+          <p>
+            <b>客户端：</b>
+            {row.client}
+          </p>
+        </>
+      );
+    }
+  },
+  {
+    title: '熔断',
+    key: 'circuitBreaker',
+    width: 150,
+    render: row => {
+      let leftLimitAmount;
+      if (row.circuitBreaker.limitAmount > 0) {
+        leftLimitAmount = (
+          <p>
+            <b>剩余次数：</b>
+            {row.circuitBreaker.leftLimitAmount}次
+          </p>
+        );
+      }
+      let availableBeginTime;
+      if (new Date().getTime() / 1000 < row.circuitBreaker.availableBeginTime) {
+        availableBeginTime = (
+          <p>
+            <b>熔断解除时间：</b>
+            {new Date(row.circuitBreaker.availableBeginTime * 1000).toLocaleString()}
+          </p>
+        );
+      }
+      return (
+        <>
+          <p>
+            <b>熔断条件：</b>
+            {row.circuitBreaker.limitAmount > 0
+              ? `${row.circuitBreaker.limitAmount}次/${getRateLimitLabel(row.circuitBreaker.limitUnit)}`
+              : '未启用'}
+          </p>
+          {leftLimitAmount}
+          <p>
+            <b>熔断时长：</b>
+            {timespanHuman(row.circuitBreaker.breakDuration)}
+          </p>
+          {availableBeginTime}
+        </>
+      );
+    }
+  },
+  {
+    title: '操作',
+    key: 'action',
+    width: 150,
+    render: (row, rowIndex) => (
+      <n-space justify="center">
+        <n-switch
+          v-model:value={row.enable}
+          on-update:value={async (value: boolean) => {
+            dialog.warning({
+              title: '询问',
+              content: `是否${value ? '启用' : '禁用'}？`,
+              positiveText: '确定',
+              negativeText: '取消',
+              onPositiveClick: async () => {
+                config.value['config:openai'].config.apis[rowIndex].enable = value;
+                const configSaveData = {
+                  'config:openai': config.value['config:openai'].config
+                };
+                const { data } = await saveConfig(configSaveData);
+                if (data?.code === 0) {
+                  row.enable = value;
+                  window.$message?.info('保存成功');
+                }
+              }
+            });
+          }}
+          v-slots={{
+            checked: () => '启用',
+            unchecked: () => '禁用'
+          }}
+        ></n-switch>
+      </n-space>
+    )
+  }
+]);
 
 onMounted(async () => {
   const { data } = await getConfig();
